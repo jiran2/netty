a7bfe6a85979a718a1352305703aef5c77561e28
jiran <jiran888@163.com>
2019-11-27 17:49:10 +0800

    update

--- example/src/main/java/io/netty/example/jiran/netty/MyNettyClientHandler.java
+++ example/src/main/java/io/netty/example/jiran/netty/MyNettyClientHandler.java
@@ -10,6 +10,14 @@ import io.netty.channel.SimpleChannelInboundHandler;
  */
 public class MyNettyClientHandler extends SimpleChannelInboundHandler {
 
+    /**
+     * 每当接收数据时，都会调用这个方法
+     *
+     * @param ctx the {@link ChannelHandlerContext} which this {@link SimpleChannelInboundHandler}
+     *            belongs to
+     * @param msg the message to handle
+     * @throws Exception
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
@@ -22,5 +30,16 @@ public class MyNettyClientHandler extends SimpleChannelInboundHandler {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         ctx.close();
+    }
+
+    /**
+     * 当一个连接建立时被调用
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
     }
 }
